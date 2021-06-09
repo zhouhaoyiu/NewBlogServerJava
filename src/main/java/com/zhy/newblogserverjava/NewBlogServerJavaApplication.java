0f23815b57996dc43e47f7780218f840f450a7b1
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2021-06-09 12:33:43 +0800

    Initial commit

---
+++ src/main/java/com/zhy/newblogserverjava/NewBlogServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zhy.newblogserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBlogServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBlogServerJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhy/newblogserverjava/NewBlogServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhy.newblogserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBlogServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
