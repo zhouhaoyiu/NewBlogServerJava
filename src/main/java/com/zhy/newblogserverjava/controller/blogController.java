16b2c5923a3f94b9957d55f2f3095f096d98b8cf
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2021-06-10 14:59:54 +0800

    add addBlog

--- src/main/java/com/zhy/newblogserverjava/controller/blogController.java
+++ src/main/java/com/zhy/newblogserverjava/controller/blogController.java
@@ -4,9 +4,11 @@ package com.zhy.newblogserverjava.controller;
 import com.zhy.newblogserverjava.domain.Blog;
 import com.zhy.newblogserverjava.mapper.BlogMapper;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSON;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +42,24 @@ public class blogController {
         json.put("code", 1000);
         json.put("blog", blog);
         return json;
+    }
+    @PostMapping("addBlog")
+    public Object addBlog(HttpServletRequest request,HttpServletResponse response){
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        String content = request.getParameter("content");
+        String created = request.getParameter("created");
+        int result = blogMapper.addBlog(title,description,content,created);
+        JSONObject json = new JSONObject();
+        if (result == 0){
+            json.put("code","1001");
+            json.put("message","fail");
+        }
+        else{
+            json.put("code","1000");
+            json.put("message","success");
+        }
+        return json;
+
     }
 }
--- src/main/java/com/zhy/newblogserverjava/mapper/BlogMapper.java
+++ src/main/java/com/zhy/newblogserverjava/mapper/BlogMapper.java
@@ -1,6 +1,7 @@
 package com.zhy.newblogserverjava.mapper;
 
 import com.zhy.newblogserverjava.domain.Blog;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
@@ -10,9 +11,12 @@ import java.util.List;
 @Repository
 public interface BlogMapper {
 
-    @Select("select * from blog" )
+    @Select("select * from blog order by id desc" )
     List<Blog> selectAllBlogs();
 
     @Select("select * from blog where id =#{blogId}")
     List<Blog> getBlog(@Param("blogId")Integer blogId);
+
+    @Insert("INSERT into blog(title,description,content,created) VALUES(#{title},#{description},#{content},#{created})")
+    int addBlog(@Param("title")String title, @Param("description")String description,@Param("content")String content,@Param("created")String created);
 }
